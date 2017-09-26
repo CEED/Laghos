c9dde084be32c5241640b3c307ebace1822d99ca
dmed256 <dmed256@gmail.com>
2017-09-25 19:27:13 -0500

    Fixed array deletes

--- laghos_solver.cpp
+++ laghos_solver.cpp
@@ -492,10 +492,10 @@ void LagrangianHydroOperator::UpdateQuadratureData(const Vector &S) const
          ++z_id;
       }
    }
-   delete rho_b;
-   delete e_b;
-   delete p_b;
-   delete cs_b;
+   delete [] rho_b;
+   delete [] e_b;
+   delete [] p_b;
+   delete [] cs_b;
    quad_data_is_current = true;
 }
 
