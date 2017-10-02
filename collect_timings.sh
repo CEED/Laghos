394f13bf8d06c5d9a8cf38b47494be8a4d704d03
Vladimir Tomov <tomov2@llnl.gov>
2017-10-02 16:51:18 -0700

    Latest timing script and plotter.

--- collect_timings.sh
+++ collect_timings.sh
@@ -3,15 +3,16 @@
 options=( 'pa' 'fa' )
 
 parallel_refs=0
-nproc=4
 mesh_dim=3
-maxL2dof=200000
+maxL2dof=1000000
 
-outfile=timings
+outfile=timings_${mesh_dim}d
 
 if (( mesh_dim == 2 )); then
+  nproc=4
   mesh_file=data/square01_quad.mesh
 else
+  nproc=8
   mesh_file=data/cube01_hex.mesh
 fi
 
@@ -33,14 +34,15 @@ BEGIN { ref = 0 }
 /CG \(L2\) rate/ { l2_cg_rate = $9 }
 /Forces rate/ { forces_rate = $8 }
 /UpdateQuadData rate/ { update_quad_rate = $8 }
-END { printf("%d %d %d %d %.8f %.8f %.8f %.8f\n", order, ref, h1_dofs, l2_dofs, h1_cg_rate, l2_cg_rate, forces_rate, update_quad_rate) }'
+/Major kernels/ { total_time = $6 }
+END { printf("%d %d %d %d %.8f %.8f %.8f %.8f %.8f\n", order, ref, h1_dofs, l2_dofs, h1_cg_rate, l2_cg_rate, forces_rate, update_quad_rate, total_time) }'
 }
 
 [ -r $outfile ] && cp $outfile $outfile.bak
-echo "# order refs h1_dofs l2_dofs h1_cg_rate l2_cg_rate forces_rate update_quad_rate" > $outfile"_"${options[0]}
-echo "# order refs h1_dofs l2_dofs h1_cg_rate l2_cg_rate forces_rate update_quad_rate" > $outfile"_"${options[1]}
+echo "# order refs h1_dofs l2_dofs h1_cg_rate l2_cg_rate forces_rate update_quad_rate total_time" > $outfile"_"${options[0]}
+echo "# order refs h1_dofs l2_dofs h1_cg_rate l2_cg_rate forces_rate update_quad_rate total_time" > $outfile"_"${options[1]}
 for method in "${options[@]}"; do
-  for torder in {1..7}; do
+  for torder in {0..4}; do
     for sref in {0..10}; do
        nzones=$(( (2**mesh_dim)**(sref+1) ))
        nL2dof=$(( nzones*(torder+1)**mesh_dim ))
@@ -48,7 +50,7 @@ for method in "${options[@]}"; do
          echo "np"$nproc "Q"$((torder+1))"Q"$torder $sref"ref" $method
          echo $(run_case mpirun -np $nproc ./laghos -$method \
                        -p 0 -tf 0.5 -cfl 0.05 -vs 1 \
-                       --max_steps 10 \
+                       --max_steps 1 \
                        --mesh $mesh_file \
                        --refine-serial $sref \
                        --refine-parallel $parallel_refs \
--- rates.py
+++ rates.py
@@ -6,8 +6,9 @@ from pylab import *
 #rc('lines',  linestyle=None, marker='.', markersize=3)
 rc('legend', fontsize=10)
 
-txt_pa  = loadtxt("timings_pa");
-txt_fa  = loadtxt("timings_fa");
+txt_pa  = loadtxt("timings_3d_pa");
+txt_fa  = loadtxt("timings_3d_fa");
+txt_oc  = loadtxt("timings_3d_occa");
 
 def make_plot(column, label_prefix, line_style, txt, title=None, fig=None):
   cm=get_cmap('Set1') # 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3'
@@ -28,28 +29,35 @@ def make_plot(column, label_prefix, line_style, txt, title=None, fig=None):
     for k in range(txt.shape[0]):
       o = txt[k,0]
       if o == p:
-        dofs.append(txt[k, 3])
-        data.append(1e3*txt[k, column])
-    ax.plot(dofs, data, line_style, label=label_prefix + str(p),
+        dofs.append(txt[k, 2])
+        data.append(txt[k, 2]/txt[k, column])
+       #data.append(1e6*txt[k, column])
+    pm1 = p-1
+    ax.plot(dofs, data, line_style, label=label_prefix + 'Q' + str(p) + 'Q' + str(p-1),
             color=colors[i], linewidth=2)
 
   ax.grid(True, which='major')
   ax.grid(True, which='minor')
   ax.legend(loc='best', prop={'size':18})
-  #axis([-0.005, 0.2, -0.15, 2.2])
-  ax.set_xlabel('Points per node', fontsize=15)
+  ax.set_autoscaley_on(False)
+  ax.set_autoscalex_on(False)
+  axis([10, 1e7, 1e4, 3e6])
+ #axis([10, 1e7, 1e5, 1e9])
+  ax.set_xlabel('H1 DOFs', fontsize=18)
   ax.set_xscale('log', basex = 10)
-  ax.set_ylabel('[DOFs x iterations] / [nodes x seconds]', fontsize=15)
+  ax.set_ylabel('[DOFs x time steps] / [seconds]', fontsize=18)
   ax.set_yscale('log', basex = 10)
+  xticks(fontsize = 18, rotation = 0)
+  yticks(fontsize = 18, rotation = 0)
   if title is not None:
-    ax.set_title(title)
+    ax.set_title(title, fontsize=18)
   return fig
 
-f1 = make_plot(4, 'PA: p = ', 'o-', txt_pa, title='CG Execution Rate')
-#f1.savefig('CG_rate.pdf', bbox_inches='tight')
-f2 = make_plot(6, 'PA: p = ', 'o-', txt_pa, title='Force Execution Rate')
-#f2.savefig('Force_rate.pdf', bbox_inches='tight')
-f3 = make_plot(4, 'FA: p = ', 'o-', txt_fa, title='CG Execution Rate')
-f4 = make_plot(6, 'FA: p = ', 'o-', txt_fa, title='Force Execution Rate')
+f1 = make_plot(8, 'PA: ', 'o-', txt_pa, title='Total Rate')
+f2 = make_plot(8, 'FA: ', 'o-', txt_fa, title='Total Rate')
+f3 = make_plot(8, 'OCCA: ', 'o-', txt_oc, title='Total Rate')
+#f1.savefig('laghos_3D_TT_PA.png', dpi=300, bbox_inches='tight')
+#f2.savefig('laghos_3D_TT_FA.png', dpi=300, bbox_inches='tight')
+#f3.savefig('laghos_3D_TT_OC.png', dpi=300, bbox_inches='tight')
 
 show()
