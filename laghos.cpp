4ed24cbcb6b705e625a1641660f1917eaa4a9974
Vladimir Tomov <tomov2@llnl.gov>
2018-10-31 12:00:57 -0700

    Added 3D mesh with 36 elements and partition options.

--- laghos.cpp
+++ laghos.cpp
@@ -44,12 +44,15 @@
 //
 // Combinations resulting in 3D uniform Cartesian MPI partitionings of the mesh:
 // -m data/cube01_hex.mesh   -pt 211 for  2 / 16 / 128 / 1024 ... tasks.
+// -m data/cube_922_hex.mesh -pt 921 for    / 18 / 144 / 1152 ... tasks.
 // -m data/cube_522_hex.mesh -pt 522 for    / 20 / 160 / 1280 ... tasks.
 // -m data/cube_12_hex.mesh  -pt 311 for  3 / 24 / 192 / 1536 ... tasks.
 // -m data/cube01_hex.mesh   -pt 221 for  4 / 32 / 256 / 2048 ... tasks.
+// -m data/cube_922_hex.mesh -pt 922 for    / 36 / 288 / 2304 ... tasks.
 // -m data/cube_522_hex.mesh -pt 511 for  5 / 40 / 320 / 2560 ... tasks.
 // -m data/cube_12_hex.mesh  -pt 321 for  6 / 48 / 384 / 3072 ... tasks.
 // -m data/cube01_hex.mesh   -pt 111 for  8 / 64 / 512 / 4096 ... tasks.
+// -m data/cube_922_hex.mesh -pt 911 for  9 / 72 / 576 / 4608 ... tasks.
 // -m data/cube_522_hex.mesh -pt 521 for 10 / 80 / 640 / 5120 ... tasks.
 // -m data/cube_12_hex.mesh  -pt 322 for 12 / 96 / 768 / 6144 ... tasks.
 
@@ -220,6 +223,18 @@ int main(int argc, char *argv[])
       case 522: // 3D.
          unit = floor(pow(num_tasks / 20, 1.0 / 3) + 1e-2);
          nxyz[0] = 5 * unit; nxyz[1] = 2 * unit; nxyz[2] = 2 * unit;
+         break;
+      case 911: // 3D.
+         unit = floor(pow(num_tasks / 9, 1.0 / 3) + 1e-2);
+         nxyz[0] = 9 * unit; nxyz[1] = unit; nxyz[2] = unit;
+         break;
+      case 921: // 3D.
+         unit = floor(pow(num_tasks / 18, 1.0 / 3) + 1e-2);
+         nxyz[0] = 9 * unit; nxyz[1] = 2 * unit; nxyz[2] = unit;
+         break;
+      case 922: // 3D.
+         unit = floor(pow(num_tasks / 36, 1.0 / 3) + 1e-2);
+         nxyz[0] = 9 * unit; nxyz[1] = 2 * unit; nxyz[2] = 2 * unit;
          break;
       default:
          if (myid == 0)
