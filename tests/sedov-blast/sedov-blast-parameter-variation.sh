0053c27ce5c39c60dbf24cd189a93a9495fb4dc1
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2020-08-19 18:01:33 -0700

    fix tests for parameter variation

---
+++ tests/sedov-blast/sedov-blast-parameter-variation.sh
@@ -0,0 +1,12 @@
+NUM_PARALLEL_PROCESSORS=8
+testNames=(fom online)
+case $subTestNum in
+  1)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -offline -romsvds -romos -romsrhs -bef 1.0 -rpar 0
+    ./merge -nset 1 -rhs
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -bef 0.5 -writesol -visit
+    ;;
+  2)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -online -rdimx 32 -rdimv 92 -rdime 26 -rdimfv 200 -rdimfe 60 -romhr -romos -sfacx 1 -sfacv 32 -sface 32 -soldiff -romrmass -romsrhs -bef 1.0
+    ;;
+esac
