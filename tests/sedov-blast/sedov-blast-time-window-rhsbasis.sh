112a3817bdfe116e605902f9d442f7d508cc973c
Dylan Copeland <copeland11@llnl.gov>
2020-08-25 13:21:03 -0700

    Adding new regression tests.

---
+++ tests/sedov-blast/sedov-blast-time-window-rhsbasis.sh
@@ -0,0 +1,16 @@
+NUM_PARALLEL_PROCESSORS=4
+testNames=(offline online romhr restore)
+case $subTestNum in
+  1)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.05 -offline -ef 0.9999 -writesol -romsvds -romos -romsrhs -nwinsamp 60
+    ;;
+  2)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romsvds -romos -sfacx 10 -sfacv 10 -sface 10 -visdiff 109 -romsrhs -nwin 2 -twp twpTemp.csv -romgs
+    ;;
+  3)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romsvds -romos -sfacx 10 -sfacv 10 -sface 10 -visdiff 109 -romsrhs -nwin 2 -twp twpTemp.csv -romgs -romhr
+    ;;
+  4)
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -restore -nwin 2 -twp twpTemp.csv
+    ;;
+esac
