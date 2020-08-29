a520f2f64ba54fc44352db2589b15219cfe0a526
Dylan Copeland <copeland11@llnl.gov>
2020-08-28 17:49:13 -0700

    Changing -romrmass to -romgs in one test.

--- tests/sedov-blast/sedov-blast-parameter-variation.sh
+++ tests/sedov-blast/sedov-blast-parameter-variation.sh
@@ -7,6 +7,6 @@ case $subTestNum in
     $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -bef 0.5 -writesol -visit
     ;;
   2)
-    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -online -rdimx 32 -rdimv 92 -rdime 26 -rdimfv 200 -rdimfe 60 -romhr -romos -sfacx 1 -sfacv 32 -sface 32 -soldiff -romrmass -romsrhs -bef 1.0
+    $HEADER laghos -m data/cube01_hex.mesh -pt 211 -tf 0.1 -online -rdimx 32 -rdimv 92 -rdime 26 -rdimfv 200 -rdimfe 60 -romhr -romos -sfacx 1 -sfacv 32 -sface 32 -soldiff -romgs -romsrhs -bef 1.0
     ;;
 esac
