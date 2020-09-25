ff6f37258ac4c19a023faeadff9d948bc55a2b83
Dylan Copeland <copeland11@llnl.gov>
2020-09-24 20:08:11 -0700

    Adding -rdimx and -rdimv to Gresho -romxandv test.

--- tests/gresho-vortices/gresho-vortices-mergexv.sh
+++ tests/gresho-vortices/gresho-vortices-mergexv.sh
@@ -5,7 +5,7 @@ case $subTestNum in
     $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.1 -s 7 -pa -offline -writesol -romsvds -romos -romsrhs -romxandv
     ;;
   2)
-    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.1 -s 7 -pa -online -rdime 30 -rdimfv 29 -rdimfe 33 -romhr -romgs -sfacx 10 -sfacv 15 -sface 15 -soldiff -romos -romsrhs -romxandv -efx 0.9999
+    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.1 -s 7 -pa -online -rdimx 9 -rdimv 18 -rdime 30 -rdimfv 29 -rdimfe 33 -romhr -romgs -sfacx 10 -sfacv 15 -sface 15 -soldiff -romos -romsrhs -romxandv -efx 0.9999
     ;;
   3)
     $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.1 -s 7 -pa -restore -rdime 30 -romsrhs -romxandv -efx 0.9999
