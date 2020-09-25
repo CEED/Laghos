3dc1ddbfbdf0a70b0f12b41cdf173ad14e2d3a04
Dylan Copeland <copeland11@llnl.gov>
2020-09-25 09:09:47 -0700

    Small changes to a regression test.

--- tests/sedov-blast/sedov-blast-mergexv.sh
+++ tests/sedov-blast/sedov-blast-mergexv.sh
@@ -5,9 +5,9 @@ case $subTestNum in
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -offline -ef 0.9999 -writesol -romsvds -romos -romsrhs -romxandv
     ;;
   2)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romsvds -romos -sfacx 10 -sfacv 10 -sface 10 -soldiff -romsrhs -romgs -romhr -rdimx 24 -rdimv 60 -rdime 20 -rdimfv 114 -rdimfe 40 -romxandv -efx 0.99999
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romos -sfacx 10 -sfacv 10 -sface 10 -soldiff -romsrhs -romgs -romhr -rdimx 24 -rdimv 60 -rdime 20 -rdimfv 114 -rdimfe 40 -romxandv -efx 0.999999
     ;;
   3)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -restore -romsrhs -rdime 20 -romxandv -efx 0.99999
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -restore -romsrhs -rdimx 24 -rdimv 60 -rdime 20 -romxandv -efx 0.999999
     ;;
 esac
