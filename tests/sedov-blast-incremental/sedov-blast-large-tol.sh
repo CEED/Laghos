9914436e945f5548587594600c62c3a51f61aade
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2020-10-15 16:57:49 -0700

    adjust large sv tol test

--- tests/sedov-blast-incremental/sedov-blast-large-tol.sh
+++ tests/sedov-blast-incremental/sedov-blast-large-tol.sh
@@ -2,7 +2,7 @@ NUM_PARALLEL_PROCESSORS=8
 testNames=(offline online romhr restore)
 case $subTestNum in
   1)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -offline -writesol -svtol 1e-1
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -offline -writesol -svtol 1e-3
     ;;
   2)
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -rdimx 3 -rdimv 7 -rdime 5 -nsamx 12 -nsamv 184 -nsame 30 -soldiff
