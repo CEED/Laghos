3ffd2603a5809aa3e14a9d49b289f6447b61923b
Siu Wun Cheung <cheung26@quartz770.llnl.gov>
2020-10-26 16:44:00 -0700

    Add romos and rostype tags to merge phase in regression tests

--- tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation-interpolate.sh
+++ tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation-interpolate.sh
@@ -3,7 +3,7 @@ testNames=(fom online)
 case $subTestNum in
   1)
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.1 -offline -romsvds -romos -rostype interpolate -romsrhs -bef 1.0 -rpar 0 -nwin 2 -tw "$BASE_DIR"/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation.csv -writesol -visit
-    $MERGE -nset 1 -rhs -nwin 2 -tw "$BASE_DIR"/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation.csv
+    $MERGE -nset 1 -romos -rostype interpolate -rhs -nwin 2 -tw "$BASE_DIR"/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation.csv
     ;;
   2)
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.1 -online -romhr -romos -rostype interpolate -sfacx 1 -sfacv 32 -sface 32 -soldiff -romgs -romsrhs -bef 1.0 -nwin 2 -tw "$BASE_DIR"/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation.csv -twp twpTemp.csv
