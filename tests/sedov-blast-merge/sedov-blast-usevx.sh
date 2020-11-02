8562415688a61faf2ffc0920d5832aa74f3789f5
Kevin Huynh <kevinhkhuynh@gmail.com>
2020-11-02 11:02:58 -0800

    change rdimfv in sedov-blast-merge tests (#109)

--- tests/sedov-blast-merge/sedov-blast-usevx.sh
+++ tests/sedov-blast-merge/sedov-blast-usevx.sh
@@ -5,7 +5,7 @@ case $subTestNum in
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -offline -ef 0.9999 -writesol -romsvds -romos -rostype load -romsrhs -romvx -efx 0.999999
     ;;
   2)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romsvds -romos -rostype load -sfacx 10 -sfacv 10 -sface 10 -soldiff -romsrhs -romgs -romhr -rdimx 66 -rdime 20 -rdimfv 114 -rdimfe 40 -romvx
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.05 -online -romsvds -romos -rostype load -sfacx 10 -sfacv 10 -sface 10 -soldiff -romsrhs -romgs -romhr -rdimx 66 -rdime 20 -rdimfv 111 -rdimfe 40 -romvx
     ;;
   3)
     $LAGHOS -m data/cube01_hex.mesh -pt 211 -restore -romsrhs -rdimx 66 -rdime 20 -romvx
