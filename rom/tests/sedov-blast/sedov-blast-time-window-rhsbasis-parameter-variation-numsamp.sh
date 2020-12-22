95ccd58ba135f51f2f2b003fe61f4d427e8a93e0
Kevin Huynh <kevinhkhuynh@gmail.com>
2020-12-21 16:52:47 -0800

    Rom absolute tests (#116)
    * add debug mode to setup
    
    * change off/on to no/yes
    
    * rom absolute test script setup
    
    * add absolute tests
    
    * add documentation
    
    * remove visit option from reg tests
    
    * update absolute tests
    
    * fix absolute test parameters
    
    * passing absolute tests
    
    * use relative l2 norm
    
    * add if/else case if baselinenorm is 0
    
    * fix test thresholds

--- rom/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation-numsamp.sh
+++ rom/tests/sedov-blast/sedov-blast-time-window-rhsbasis-parameter-variation-numsamp.sh
@@ -2,7 +2,7 @@ NUM_PARALLEL_PROCESSORS=8
 testNames=(fom online)
 case $subTestNum in
   1)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.1 -offline -romsvds -romos -rostype previous -romsrhs -bef 1.0 -rpar 0 -writesol -visit
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.1 -offline -romsvds -romos -rostype previous -romsrhs -bef 1.0 -rpar 0 -writesol
     $MERGE -nset 1 -romos -rostype previous -rhs -nwinsamp 125 -nwinover 30
     ;;
   2)
