8be41bb7b9a94f6732c221f083c4bc167a42e8f7
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2021-01-19 17:54:39 -0800

    use rand_svd parameters in test

--- rom/tests/sedov-blast-randomized/sedov-blast-randomized.sh
+++ rom/tests/sedov-blast-randomized/sedov-blast-randomized.sh
@@ -2,18 +2,18 @@ NUM_PARALLEL_PROCESSORS=8
 testNames=(offline online romhr qdeim restore)
 case $subTestNum in
   1)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -offline -writesol -romsvdrm
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -offline -writesol -romsvdrm -randdimx 2 -randdimv 12 -randdime 6
     ;;
   2)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -rdimx 2 -rdimv 12 -rdime 6 -sfacx 6 -sfacv 20 -sface 2 -soldiff
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -rdimx 2 -rdimv 11 -rdime 5 -sfacx 6 -sfacv 20 -sface 2 -soldiff
     ;;
   3)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -romhr -rdimx 2 -rdimv 12 -rdime 6 -sfacx 6 -sfacv 20 -sface 2 -soldiff
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -romhr -rdimx 2 -rdimv 11 -rdime 5 -sfacx 6 -sfacv 20 -sface 2 -soldiff
     ;;
   4)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -rdimx 2 -rdimv 12 -rdime 6 -romhr -qdeim -soldiff
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -rdimx 2 -rdimv 11 -rdime 5 -romhr -qdeim -soldiff
     ;;
   5)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -restore -rdimx 2 -rdimv 12 -rdime 6
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -restore -rdimx 2 -rdimv 11 -rdime 5
     ;;
 esac
