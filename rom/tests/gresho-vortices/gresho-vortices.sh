a5d0c6a72649ddeea44de940e4842df8eda31111
Siu Wun Cheung <cheung26@quartz1922.llnl.gov>
2021-03-16 16:42:14 -0700

    Fix a typo

--- rom/tests/gresho-vortices/gresho-vortices.sh
+++ rom/tests/gresho-vortices/gresho-vortices.sh
@@ -9,7 +9,7 @@ case $subTestNum in
     ;;
   3)
     $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -rdimfv 34 -rdimfe 39 -romhrprep -nsamx 18 -nsamv 3401 -nsame 128 -romsrhs
-    $LAGHOS_SERIAL -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -rdimefv 34 -rdimfe 39 -romhr -nsamx 18 -nsamv 3401 -nsame 128 -romsrhs
+    $LAGHOS_SERIAL -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -rdimfv 34 -rdimfe 39 -romhr -nsamx 18 -nsamv 3401 -nsame 128 -romsrhs
     ;;
   4)
     $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -rdimfv 34 -rdimfe 39 -romhrprep -sfacv 40 -sface 40 -qdeim -romsrhs
