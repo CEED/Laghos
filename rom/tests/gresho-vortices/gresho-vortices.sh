c13e9643c7580de4820de043bdfc5f75260f0c1c
siuwuncheung <61989266+siuwuncheung@users.noreply.github.com>
2021-03-24 07:24:52 -0700

    Revert "Remove -romsrhs tag in regression test scripts"
    This reverts commit 0200d93c26cf67e86fd5865c18ba3c2fde9773cf.

--- rom/tests/gresho-vortices/gresho-vortices.sh
+++ rom/tests/gresho-vortices/gresho-vortices.sh
@@ -2,7 +2,7 @@ NUM_PARALLEL_PROCESSORS=8
 testNames=(offline online romhr qdeim restore)
 case $subTestNum in
   1)
-    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -offline -writesol -romsvds
+    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -offline -writesol -romsvds -romsrhs
     ;;
   2)
     $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -soldiff
@@ -16,6 +16,6 @@ case $subTestNum in
     $LAGHOS_SERIAL -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -online -romgs -rdimx 4 -rdimv 20 -rdime 16 -rdimfv 34 -rdimfe 39 -romhr -sfacv 40 -sface 40 -qdeim
     ;;
   5)
-    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -restore -rdimx 4 -rdimv 20 -rdime 16 -soldiff
+    $LAGHOS -p 4 -m data/square_gresho.mesh -rs 3 -ok 3 -ot 2 -tf 0.12 -s 7 -pa -restore -rdimx 4 -rdimv 20 -rdime 16 -soldiff -romsrhs
     ;;
 esac
