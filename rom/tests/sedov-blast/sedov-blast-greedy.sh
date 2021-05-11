08174217f4789bdd50b0df36dcb91dc57e3fd846
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2021-05-11 14:14:17 -0700

    update sedov blast greedy test

--- rom/tests/sedov-blast/sedov-blast-greedy.sh
+++ rom/tests/sedov-blast/sedov-blast-greedy.sh
@@ -5,9 +5,9 @@ case $subTestNum in
     $GREEDY $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -build-database -writesol -romsvds -greedy-param-min 0.5 -greedy-param-max 2.5 -greedy-param-size 5 -greedysubsize 2 -greedyconvsize 3
     ;;
   2)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -use-database -bef 0.9 -online -rdimx 2 -rdimv 12 -rdime 16 -nsamx 12 -nsamv 184 -nsame 30
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -use-database -bef 0.9 -online -rdimx 7 -rdimv 12 -rdime 6 -nsamx 12 -nsamv 184 -nsame 30
     ;;
   3)
-    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -use-database -bef 0.9 -restore -rdimx 2 -rdimv 12 -rdime 16 -soldiff
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -use-database -bef 0.9 -restore -rdimx 7 -rdimv 12 -rdime 6
     ;;
 esac
