97b2004ac11f271017b867a65c6feb266e708b38
Siu Wun Cheung <cheung26@quartz188.llnl.gov>
2021-06-17 11:09:46 -0700

    Add MFEM_Verify for HydroODESolver::sampler

--- rom/laghos_timeinteg.cpp
+++ rom/laghos_timeinteg.cpp
@@ -45,11 +45,13 @@ void HydroODESolver::Init(TimeDependentOperator &_f)
 void HydroODESolver::SetSampler(ROM_Sampler &_f)
 {
     sampler = dynamic_cast<ROM_Sampler *>(f);
+    MFEM_VERIFY(sampler, "HydroSolvers expect ROM_Sampler.");
 }
 
 void HydroODESolver::SetSamplerLast(ROM_Sampler &_f)
 {
     samplerLast = dynamic_cast<ROM_Sampler *>(f);
+    MFEM_VERIFY(samplerLast, "HydroSolvers expect ROM_Sampler.");
 }
 
 void RK2AvgSolver::Step(Vector &S, double &t, double &dt)
@@ -89,6 +91,7 @@ void RK2AvgSolver::Step(Vector &S, double &t, double &dt)
     // S = S0 + 0.5 * dt * dS_dt;
     add(S0, 0.5 * dt, dS_dt, S);
     if (sampler) sampler->SampleSolution(t, dt, S);
+    else std::cout << "No sample for intemediate RK stages" << endl;
     if (samplerLast) samplerLast->SampleSolution(t, dt, S);
     hydro_oper->ResetQuadratureData();
     hydro_oper->UpdateMesh(S);
