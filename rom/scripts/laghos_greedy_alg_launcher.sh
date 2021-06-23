940386adcf3d582afa359c0392514e5cd8222e8b
rollbackcc <kevinhkhuynh@gmail.com>
2021-06-23 12:56:57 -0700

    add parallel version of script

--- rom/scripts/laghos_greedy_alg_launcher.sh
+++ rom/scripts/laghos_greedy_alg_launcher.sh
@@ -6,9 +6,29 @@
 #SBATCH -o greedy.log
 #SBATCH --open-mode truncate
 
+PARALLEL=${PARALLEL:-1}
+
+# Check machine
+case "$(uname -s)" in
+    Linux*)
+		  PARALLEL_COMMAND="srun -p pdebug -n $PARALLEL $@"
+          SERIAL_COMMAND="srun -p pdebug -n 1 $@";;
+    Darwin*)
+		  PARALLEL_COMMAND="mpirun -oversubscribe -n $PARALLEL $@"
+          SERIAL_COMMAND="mpirun -oversubscribe -n 1 $@";;
+    *)
+		  echo "The regression tests can only run on Linux and MAC."
+		  exit 1
+esac
+
 # Check if greedy algorithm has ended
 while [ "$?" -eq 0 ]
 do
-    "$@"
+    if [[ -f "../run/hyperreduce.txt" ]]; then
+        "$SERIAL_COMMAND"
+        rm ../run/hyperreduce.txt
+    else
+        "$PARALLEL_COMMAND"
+    fi
 done
 exit 0
