adbed2a3d9d39732419adad6c4ecd6317eb159f4
Kevin <huynh24@llnl.gov>
2022-06-07 10:51:48 -0700

    Add sopt test

---
+++ rom/tests/sedov-blast/sedov-blast-time-window-sopt.sh
@@ -0,0 +1,17 @@
+NUM_PARALLEL_PROCESSORS=8
+testNames=(offline online romhr restore)
+case $subTestNum in
+  1)
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -offline -ef 0.9999 -nwin 4 -tw "$BASE_DIR"/tests/sedov-blast/sedov-blast-time-window.csv -writesol -romsvds -twp twpTemp.csv -no-romsns -romos
+    ;;
+  2)
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -romgs -nwin 4 -twp twpTemp.csv -soldiff -no-romsns -romos -sopt
+    ;;
+  3)
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -romgs -romhrprep -nwin 4 -twp twpTemp.csv -no-romsns -romos -sopt
+    $LAGHOS_SERIAL -m data/cube01_hex.mesh -pt 211 -tf 0.01 -online -romgs -romhr -nwin 4 -twp twpTemp.csv -no-romsns -romos -sopt
+    ;;
+  4)
+    $LAGHOS -m data/cube01_hex.mesh -pt 211 -restore -nwin 4 -twp twpTemp.csv -soldiff -no-romsns -romos
+    ;;
+esac
