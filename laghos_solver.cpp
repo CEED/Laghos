f830a46e22577e741ae4d2d1d28a6325b383faf9
Andrew Ho <ho37@llnl.gov>
2024-10-01 16:38:20 -0700

    make work with CMake and installed external mfem

--- laghos_solver.cpp
+++ laghos_solver.cpp
@@ -14,9 +14,9 @@
 // software, applications, hardware, advanced system engineering and early
 // testbed platforms, in support of the nation's exascale computing imperative.
 
-#include "general/forall.hpp"
+#include "mfem/general/forall.hpp"
 #include "laghos_solver.hpp"
-#include "linalg/kernels.hpp"
+#include "mfem/linalg/kernels.hpp"
 #include <unordered_map>
 
 #ifdef MFEM_USE_MPI
